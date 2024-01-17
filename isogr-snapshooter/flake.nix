4908f9091454fe8d52429c0090247266a76e2ca2
Jeffrey Lau <jeffrey.lau@ribose.com>
2024-01-17 16:53:07 +0800

    feat: Add isogr-snapshooter

---
+++ isogr-snapshooter/flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Scraper for geodetic.isotc211.org";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell/main";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , devshell
+    , flake-compat
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      cwd = builtins.toString ./.;
+      overlays = map (x: x.overlays.default) [
+        devshell
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      python = pkgs.python311.withPackages (pkgs: with pkgs; [
+        beautifulsoup4
+        mypy
+        pylsp-mypy
+        python-lsp-ruff
+        requests
+        ruff-lsp
+        types-beautifulsoup4
+        types-requests
+        typing-extensions
+      ]);
+    in
+    rec {
+
+      # nix develop
+      devShell = pkgs.devshell.mkShell {
+        env = [
+        ];
+        commands = [
+          {
+            name = "serve";
+            command = "scripts/serve \"$@\"";
+            help = "Serve the site";
+            category = "Scraper";
+          }
+          {
+            name = "scrape";
+            command = "scripts/scrape \"$@\"";
+            help = "Scrape the site's HTML, JSON, static assets, etc.";
+            category = "Scraper";
+          }
+        ];
+        packages = with pkgs; [
+          asdf-vm
+          bash
+          curl
+          fd
+          fzf
+          gnused
+          jq
+          nodejs_18
+          ripgrep
+          wget
+          yq
+        ] ++ [
+          python
+        ];
+      };
+    });
+}
