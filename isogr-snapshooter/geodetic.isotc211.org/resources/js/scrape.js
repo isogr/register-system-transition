0960134182d55453c5abf1d7b1bbc474cbd85c93
ribose-jeffreylau <ribose-jeffreylau@users.noreply.github.com>
2024-02-29 03:10:37 +0000

    chore(snapshot): Update ISOGR snapshot (created by automated workflow)

--- isogr-snapshooter/geodetic.isotc211.org/resources/js/scrape.js
+++ isogr-snapshooter/geodetic.isotc211.org/resources/js/scrape.js
@@ -1,8 +1,8 @@
 function s(i) { return (new Date(i * 1000).toISOString()).split(/\.\d*/).join(''); }
-var scrapeStart = s(1709086504);
-var scrapeEnd = s(1709094062);
+var scrapeStart = s(1709172773);
+var scrapeEnd = s(1709176208);
 var scrapeText = '<span id="scrape-timing">';
-scrapeText += '<a href="https://github.com/isogr/register-system-transition/tree/main/isogr-snapshooter" target="_blank">Page scraped</a> from <span title="1709086504">' + scrapeStart + '</span> to <span title="1709094062">' + scrapeEnd + '</span>';
+scrapeText += '<a href="https://github.com/isogr/register-system-transition/tree/main/isogr-snapshooter" target="_blank">Page scraped</a> from <span title="1709172773">' + scrapeStart + '</span> to <span title="1709176208">' + scrapeEnd + '</span>';
 scrapeText += ' by <a href="https://github.com/isogr/register-system-transition/tree/main/isogr-snapshooter" target="_blank">isogr-snapshooter</a>.';
 scrapeText += '</span>';
 document.querySelector('footer p.text-muted').innerHTML += '<span>(' + scrapeText + ')</span>';
